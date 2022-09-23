3c952c196bc6e88e94aef49c547392e7c90a4b39
Mahmud Hasan Imran <mahmud.bubt.150@gmail.com>
2022-09-23 12:51:52 +0600

    Update Index.js

--- index.js
+++ index.js
@@ -11,7 +11,6 @@ require('dotenv').config()
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@lateef-creation-portfol.s0jnuyt.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
-
 async function run() {
     try {
       await client.connect();
