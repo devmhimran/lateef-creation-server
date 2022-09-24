82e1e65f5359c367065ecc3fa8d231cf67c1c299
Mahmud Hasan Imran <mahmud.bubt.150@gmail.com>
2022-09-24 16:45:11 +0600

    Update MongoDB

--- index.js
+++ index.js
@@ -8,8 +8,9 @@ app.use(cors())
 app.use(express.json())
 require('dotenv').config()
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@lateef-creation-portfol.s0jnuyt.mongodb.net/?retryWrites=true&w=majority`;
+const uri = "mongodb+srv://lateef-creation:01534871992@cluster0.q4n1u3x.mongodb.net/?retryWrites=true&w=majority";
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
 
 async function run() {
     try {
