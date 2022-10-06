c1a876c96aa71bc30bf26c56a15ac320964bb2ef
Mahmud Hasan Imran <mahmud.bubt.150@gmail.com>
2022-10-06 06:49:29 +0600

    Update Run Function

--- index.js
+++ index.js
@@ -12,32 +12,34 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@lateef-
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 
- function run() {
+async function run() {
+  try {
+    await client.connect();
 
-       client.connect();
+    const portfolioUpload = client.db('portfolio').collection('portfolio-data');
+    app.get('/portfolio-data', async (req, res) => {
+      const query = {};
+      const cursor = portfolioUpload.find(query);
+      const data = await cursor.toArray();
+      res.send(data);
+    });
 
-      const portfolioUpload = client.db('portfolio').collection('portfolio-data');
-      app.get('/portfolio-data', async (req,res) =>{
-        const query = {};
-        const cursor = portfolioUpload.find(query);
-        const data = await cursor.toArray();
-        res.send(data);
-      });
+    app.post('/portfolio-upload', (req, res) => {
+      const addData = req.body;
+      const result = portfolioUpload.insertOne(addData)
+      res.send(result)
 
-      app.post('/portfolio-upload', (req, res) =>{
-        const addData = req.body;
-        const result = portfolioUpload.insertOne(addData)
-        res.send(result)
-  
-      })
-      
+    })
+  }  finally {
+    // await client.close();
   }
-  run()
+  }
+run().catch(console.dir);
 
-  app.get('/', (req, res) => {
-    res.send('Welcome to Lateef Creation V4')
-  })
-  
-  app.listen(port, () => {
-    console.log(`Port - ${port}`)
-  })
+app.get('/', (req, res) => {
+  res.send('Welcome to Lateef Creation V4')
+})
+
+app.listen(port, () => {
+  console.log(`Port - ${port}`)
+})
