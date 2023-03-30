66fc832aa73890b2fff6a13d7e23cd4b96b49eea
Mahmud Hasan Imran <mahmud.bubt.150@gmail.com>
2023-03-31 00:48:15 +0600

    Update MongoDB and Added JWT

--- index.js
+++ index.js
@@ -2,33 +2,72 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 5000;
 var cors = require('cors')
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 app.use(cors())
 app.use(express.json())
 require('dotenv').config()
 
+const jwt = require('jsonwebtoken');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@lateef-creation-portfol.s0jnuyt.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 
+function verifyJWT(req, res, next) {
+  const authHeader = req.headers.authorization;
+  if (!authHeader) {
+    return res.status(401).send({ message: 'UnAuthorized access' });
+  }
+  const token = authHeader.split(' ')[1];
+  jwt.verify(token, process.env.ACCESS_TOKEN, function (err, decoded) {
+    if (err) {
+      return res.status(403).send({ message: 'forbidden access' })
+    }
+    req.decoded = decoded;
+    next();
+  });
+}
+
 async function run() {
   try {
 
-    const portfolioUpload = client.db('portfolio').collection('portfolio-data');
+    const portfolioData = client.db('portfolio').collection('portfolio-data');
+    const userCollection = client.db('portfolio').collection('user');
+
     app.get('/portfolio-data', async (req, res) => {
       const query = {};
-      const cursor = await portfolioUpload.find(query);
+      const cursor = await portfolioData.find(query);
       const data = await cursor.toArray();
       res.send(data);
     });
 
-    app.post('/portfolio-upload', (req, res) => {
+    app.post('/portfolio-upload', verifyJWT, (req, res) => {
       const addData = req.body;
-      const result = portfolioUpload.insertOne(addData)
+      const result = portfolioData.insertOne(addData)
       res.send(result)
 
     })
+
+    app.delete('/portfolio-data/:id', verifyJWT, async(req, res) =>{
+      const id = req.params.id;
+      const query = {_id: ObjectId(id)};
+      const result = await portfolioData.deleteOne(query);
+      res.send(result);
+    })
+
+    app.put('/user/:email', async (req, res) => {
+      const email = req.params.email;
+      const user = req.body;
+      const filter = { email: email };
+      const options = { upsert: true };
+      const updateDoc = {
+        $set: user,
+      };
+      const result = await userCollection.updateOne(filter, updateDoc, options);
+      const token = jwt.sign({ email: email }, process.env.ACCESS_TOKEN, { expiresIn: '24h' });
+      res.send({ result, token });
+    })
+
   }  finally {
     // await client.close();
   }
